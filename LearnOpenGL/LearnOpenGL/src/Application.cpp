74cd6aa2fda9482db00eb40851631ec3d5f3fc0a
Adam Harris <adam_harris@hotmail.com>
2021-08-26 15:27:24 +0100

    Vertex Attrib Array

--- LearnOpenGL/LearnOpenGL/src/Application.cpp
+++ LearnOpenGL/LearnOpenGL/src/Application.cpp
@@ -1,51 +1,68 @@
 #include <gl/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "Application.h"
 
-int main(void)
+GLFWwindow* _pWindow = nullptr;
+
+void Initialize()
 {
     glewInit();
 
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
-        return -1;
+        return;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
-    if (!window)
+    _pWindow = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+    if (!_pWindow)
     {
         glfwTerminate();
-        return -1;
+        return;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(_pWindow);
 
     GLenum err = glewInit();
     if (err != GLEW_OK) {
         std::cout << "[ERROR]: With setting up GLEW" << std::endl;
     }
+}
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
+
+int main(void)
+{
+    Initialize();
+
+
+    float position[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(_pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        glVertex2f(-0.33f, -0.33f);
-        glVertex2f(0.0f, 0.33f);
-        glVertex2f(0.33f, -0.33f);
-
-        glEnd();
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(_pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
@@ -54,3 +71,5 @@ int main(void)
     glfwTerminate();
     return 0;
 }
+
+
---
+++ LearnOpenGL/LearnOpenGL/src/Application.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int Initialize(bool& retflag);
