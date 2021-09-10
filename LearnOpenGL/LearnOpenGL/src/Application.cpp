186e00fe0a8dd54164894a5a5e8c15cd5de25e0f
Adam Harris <adam_harris@hotmail.com>
2021-09-10 10:27:22 +0100

    Refactored rendering to make multiple passes easier

--- LearnOpenGL/LearnOpenGL/src/Application.cpp
+++ LearnOpenGL/LearnOpenGL/src/Application.cpp
@@ -31,8 +31,21 @@ float lastY = 360;
 
 Camera* camera;
 
+std::vector<PointLight> pointLights;
+glm::vec3 lightPos(-2.0, 4.0, -1.0);
+
+unsigned int crateDiffuse;
+unsigned int crateSpecular;
+unsigned int floorDiffuse;
+unsigned int floorSpecular;
+
 void MouseCallback(GLFWwindow* window, double xpos, double ypos);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+void RenderCube();
+void RenderQuad();
+void RenderScene(Shader* shader);
+void RenderSkybox();
+void RenderLights();
 
 void BindTexture(GLenum slot, GLenum target, unsigned int& textureID) {
 	GLCall(glActiveTexture(slot));
@@ -98,54 +111,18 @@ int main(void)
 {
 	Initialize();
 
-	unsigned int skyboxVAO;
-	unsigned int skyboxVBO;
-
-	glGenVertexArrays(1, &skyboxVAO);
-	glBindVertexArray(skyboxVAO);
-
-	glGenBuffers(1, &skyboxVBO);
-	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
-	GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0));
-	GLCall(glEnableVertexAttribArray(0));
-
-	unsigned int cubeVao;
-	unsigned int vbo;
-	CreateBuffers(cubeVao, vbo, cubeVertices, sizeof(cubeVertices) / sizeof(float));
-
-	GLCall(glUseProgram(0));
-	GLCall(glBindVertexArray(0));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
-
-	unsigned int quadVAO;
-	unsigned int quadVBO;
-	CreateBuffers(quadVAO, quadVBO, quadVerts, sizeof(quadVerts) / sizeof(float));
 
 
 
 	GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 
 	TextureLoader textureLoader;
-	unsigned int crateDiffuse = textureLoader.LoadTexture("Res/Textures/container2.png");
-	unsigned int crateSpecular = textureLoader.LoadTexture("Res/Textures/container2_specular.png");
-	unsigned int floorDiffuse = textureLoader.LoadTexture("Res/Textures/floor.png");
-	unsigned int floorSpecular = textureLoader.LoadTexture("Res/Textures/floorSpecular.png");
+	crateDiffuse = textureLoader.LoadTexture("Res/Textures/container2.png");
+	crateSpecular = textureLoader.LoadTexture("Res/Textures/container2_specular.png");
+	floorDiffuse = textureLoader.LoadTexture("Res/Textures/floor.png");
+	floorSpecular = textureLoader.LoadTexture("Res/Textures/floorSpecular.png");
 
-	std::vector<std::string> faces{
-		"Res/Textures/skybox/right.jpg",
-		"Res/Textures/skybox/left.jpg",
-		"Res/Textures/skybox/top.jpg",
-		"Res/Textures/skybox/bottom.jpg",
-		"Res/Textures/skybox/front.jpg",
-		"Res/Textures/skybox/back.jpg",
-	};
 
-	unsigned int skybox = textureLoader.LoadCubemap(faces);
-
-	Shader* lightShader = new Shader("Res/Shaders/LightShader.vert", "Res/Shaders/LightShader.frag");
-	Shader* skyboxShader = new Shader("Res/Shaders/Skybox.vert", "Res/Shaders/Skybox.frag");
 	Shader* advancedLightingShader = new Shader("Res/Shaders/AdvancedLighting.vert", "Res/Shaders/AdvancedLighting.frag");
 	Shader* depthShader = new Shader("Res/Shaders/RenderToDepthMap.vert", "Res/Shaders/RenderToDepthMap.frag");
 	Shader* framebufferShader = new Shader("Res/Shaders/Framebuffer.vert", "Res/Shaders/Framebuffer.frag");
@@ -176,8 +153,6 @@ int main(void)
 
 
 
-	glm::vec3 lightPos(-2.0, 4.0, -1.0);
-
 	camera = new Camera(_pWindow, width, height, 45.0f);
 
 	glm::vec3 lightDiffuse = glm::vec3(1.0f);
@@ -186,7 +161,7 @@ int main(void)
 
 	DirectionalLight sun(glm::vec3(-0.2f, -1.0f, -0.3f), lightAmbient, lightDiffuse, lightSpecular);
 
-	std::vector<PointLight> pointLights;
+	
 	pointLights.push_back(PointLight(glm::vec3(0.2f, 0.2f, 2.0f), lightAmbient, lightDiffuse, lightSpecular, 1.0f, 0.09f, 0.032f));
 	pointLights.push_back(PointLight(glm::vec3(2.3f, -3.3f, -4.0f), lightAmbient, lightDiffuse, lightSpecular, 1.0f, 0.09f, 0.032f));
 	pointLights.push_back(PointLight(glm::vec3(-4.0f, 2.0f, -12.0f), lightAmbient, lightDiffuse, lightSpecular, 1.0f, 0.09f, 0.032f));
@@ -241,30 +216,8 @@ int main(void)
 
 		glCullFace(GL_FRONT);
 
-		//Draw Cubes
-		GLCall(glBindVertexArray(cubeVao));
-		BindTexture(GL_TEXTURE0, GL_TEXTURE_2D, crateDiffuse);
-		for (unsigned int i = 0; i < 10; i++) {
-			model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			depthShader->SetMatrix4("model", model);
-			GLCall(glDrawArrays(GL_TRIANGLES, 0, 36));
-		}
+		RenderScene(depthShader);
 
-		//move floor plane
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(6.25f, 0.0f, -12.5f));
-		model = glm::scale(model, glm::vec3(25.0f, 1.0f, 25.0f));
-		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-		depthShader->SetMatrix4("model", model);
-
-
-
-		//Draw Floor
-		BindTexture(GL_TEXTURE0, GL_TEXTURE_2D, floorDiffuse);
-		glBindVertexArray(quadVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		
 		glCullFace(GL_BACK); // don't forget to reset original culling face
 
 		//Second Pass: Normal Drawing
@@ -278,70 +231,17 @@ int main(void)
 		framebufferShader->SetFloat("far_plane", far_plane);
 
 		//Draw Skybox
-		glDepthMask(false);
-		glBindVertexArray(skyboxVAO);
-		glm::mat4 skyboxView = glm::mat4(glm::mat3(camera->ViewMat()));
-		skyboxShader->SetMatrix4("projection", projection);
-		skyboxShader->SetMatrix4("view", skyboxView);
-		BindTexture(GL_TEXTURE0, GL_TEXTURE_CUBE_MAP, skybox);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		glDepthMask(true);
-
-		////Draw Light
-		//glBindVertexArray(cubeVao);
-		//lightShader->SetMatrix4("view", view);
-		//lightShader->SetMatrix4("projection", projection);
-		//for (int i = 0; i < 4; i++) {
-		//	model = glm::mat4(1.0);
-		//	model = glm::translate(model, pointLights[i].position);
-		//	model = glm::scale(model, glm::vec3(0.2));
-		//	
-		//	lightShader->SetMatrix4("model", model);
-		//	lightShader->SetFloat3("lightColor", pointLights[i].diffuse);
-		//	glDrawArrays(GL_TRIANGLES, 0, 36);
-		//}
-
-		//Draw Light
-		glBindVertexArray(cubeVao);
-		lightShader->SetMatrix4("view", view);
-		lightShader->SetMatrix4("projection", projection);
-		model = glm::mat4(1.0);
-		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2));
-
-		lightShader->SetMatrix4("model", model);
-		lightShader->SetFloat3("lightColor", pointLights[0].diffuse);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		RenderSkybox();
+		RenderLights();
 
 		shadowShader->SetInt("diffuseTexture", 0);
 		shadowShader->SetFloat3("viewPos", camera->Position());
 		shadowShader->SetMatrix4("view", view);
 		shadowShader->SetMatrix4("projection", projection);
 		shadowShader->SetMatrix4("lightSpaceMatrix", lightSpaceMatrix);
-
 		BindTexture(GL_TEXTURE1, GL_TEXTURE_2D, depthMap);
 
-		//Draw Cubes
-		GLCall(glBindVertexArray(cubeVao));
-		BindTexture(GL_TEXTURE0, GL_TEXTURE_2D, crateDiffuse);
-		for (unsigned int i = 0; i < 10; i++) {
-			model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			shadowShader->SetMatrix4("model", model);
-			GLCall(glDrawArrays(GL_TRIANGLES, 0, 36));
-		}
-
-		//move floor plane
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(6.25f, 0.0f, -12.5f));
-		model = glm::scale(model, glm::vec3(25.0f, 1.0f, 25.0f));
-		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-		shadowShader->SetMatrix4("model", model);
-
-		//Draw Floor
-		BindTexture(GL_TEXTURE0, GL_TEXTURE_2D, floorDiffuse);
-		glBindVertexArray(quadVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		RenderScene(shadowShader);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(_pWindow);
@@ -352,14 +252,130 @@ int main(void)
 		ProcessInput();
 	}
 
-	lightShader->CleanupShader();
-
-	delete lightShader;
-
-	lightShader = nullptr;
-
 	glfwTerminate();
 	return 0;
+}
+
+unsigned int cubeVAO;
+unsigned int cubeVBO;
+void RenderCube()
+{
+	if (cubeVAO == 0) {
+		CreateBuffers(cubeVAO, cubeVBO, cubeVertices, sizeof(cubeVertices) / sizeof(float));
+	}
+
+	glBindVertexArray(cubeVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+unsigned int quadVAO;
+unsigned int quadVBO;
+void RenderQuad()
+{
+	if (quadVAO == 0) {
+		CreateBuffers(quadVAO, quadVBO, quadVerts, sizeof(quadVerts) / sizeof(float));
+	}
+
+	glBindVertexArray(quadVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void RenderScene(Shader* shader)
+{
+	glm::mat4 model = glm::mat4(1.0);
+
+	//Draw Cubes
+	BindTexture(GL_TEXTURE0, GL_TEXTURE_2D, crateDiffuse);
+	for (unsigned int i = 0; i < 10; i++) {
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, cubePositions[i]);
+		shader->SetMatrix4("model", model);
+		RenderCube();
+	}
+
+	//move floor plane
+	model = glm::mat4(1.0f);
+	model = glm::translate(model, glm::vec3(6.25f, 0.0f, -12.5f));
+	model = glm::scale(model, glm::vec3(25.0f, 1.0f, 25.0f));
+	model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	shader->SetMatrix4("model", model);
+
+	//Draw Floor
+	BindTexture(GL_TEXTURE0, GL_TEXTURE_2D, floorDiffuse);
+	RenderQuad();
+}
+
+
+unsigned int skyboxVAO;
+unsigned int skyboxVBO;
+Shader* skyboxShader;
+unsigned int skyboxTexture;
+void RenderSkybox() {
+	if (skyboxVAO == 0) {
+		glGenVertexArrays(1, &skyboxVAO);
+		glBindVertexArray(skyboxVAO);
+
+		glGenBuffers(1, &skyboxVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
+		GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0));
+		GLCall(glEnableVertexAttribArray(0));
+
+		skyboxShader = new Shader("Res/Shaders/Skybox.vert", "Res/Shaders/Skybox.frag");
+
+		std::vector<std::string> faces{
+			"Res/Textures/skybox/right.jpg",
+			"Res/Textures/skybox/left.jpg",
+			"Res/Textures/skybox/top.jpg",
+			"Res/Textures/skybox/bottom.jpg",
+			"Res/Textures/skybox/front.jpg",
+			"Res/Textures/skybox/back.jpg",
+		};
+
+		TextureLoader textureLoader;
+		skyboxTexture = textureLoader.LoadCubemap(faces);
+	}
+
+	glDepthMask(false);
+	glBindVertexArray(skyboxVAO);
+	glm::mat4 skyboxView = glm::mat4(glm::mat3(camera->ViewMat()));
+	skyboxShader->SetMatrix4("projection", camera->ProjMat());
+	skyboxShader->SetMatrix4("view", skyboxView);
+	BindTexture(GL_TEXTURE0, GL_TEXTURE_CUBE_MAP, skyboxTexture);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glDepthMask(true);
+}
+
+
+Shader* lightShader;
+void RenderLights() {
+	if (lightShader == nullptr) {
+		lightShader = new Shader("Res/Shaders/LightShader.vert", "Res/Shaders/LightShader.frag");
+	}
+
+	glm::mat4 model = glm::mat4(1.0);
+
+	lightShader->SetMatrix4("view", camera->ViewMat());
+	lightShader->SetMatrix4("projection", camera->ProjMat());
+	for (int i = 0; i < 4; i++) {
+		model = glm::mat4(1.0);
+		model = glm::translate(model, pointLights[i].position);
+		model = glm::scale(model, glm::vec3(0.2));
+		
+		lightShader->SetMatrix4("model", model);
+		lightShader->SetFloat3("lightColor", pointLights[i].diffuse);
+		RenderCube();
+	}
+
+	//Draw Light
+	lightShader->SetMatrix4("view", camera->ViewMat());
+	lightShader->SetMatrix4("projection", camera->ProjMat());
+	model = glm::mat4(1.0);
+	model = glm::translate(model, lightPos);
+	model = glm::scale(model, glm::vec3(0.2));
+	lightShader->SetMatrix4("model", model);
+	lightShader->SetFloat3("lightColor", pointLights[0].diffuse);
+	RenderCube();
 }
 
 void ProcessInput()
